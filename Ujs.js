3635fa000cfcec18d08144bc0efde67f75116fc1
none <46204001+JoaoGeraldes@users.noreply.github.com>
2020-11-20 21:47:03 +0000

    First commit

---
+++ Ujs.js
@@ -0,0 +1,10 @@
+class Ujs {
+  constructor(array) {
+    this.array = array;
+    console.log(array);
+  }
+
+  cloneAndRemoveDuplicates() {}
+}
+
+module.exports = Ujs;
---
+++ index.js
@@ -0,0 +1,5 @@
+const Ujs = require("./Ujs");
+
+const mockArray = [1, 5, 7, 3, 6, 22, 7788, 325, 357, 78899, 2345, 3];
+
+const u = new Ujs(mockArray);
